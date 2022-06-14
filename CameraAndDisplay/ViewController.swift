58c524ebcba04459fbb258901badb2b05d651548
sophifang <sophiayanfang@gmail.com>
2022-06-14 10:25:42 -0700

    Initial Commit

---
+++ CameraAndDisplay/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  CameraAndDisplay
+//
+//  Created by Scholar on 6/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var imagePicker = UIImagePickerController()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func choosePhotoTapped(_ sender: Any) {
+            
+            imagePicker.sourceType = .photoLibrary
+            
+            present(imagePicker, animated: true, completion: nil)
+        }
+    
+    
+    @IBAction func selfiePressed(_ sender: Any) {
+        imagePicker.sourceType = .camera
+        
+    }
+}
+
